91851d36db12486904a32a89c23d5af61d3a5f53
Ned4ik <bohdan.vozniuk@gmail.com>
2022-05-14 18:14:16 +0300

    init

---
+++ src/Components/Page/Report/index.tsx
@@ -0,0 +1,8 @@
+export const Report: React.FC = () => {
+    return(
+        <div className="w-screen overflow-y-hidden py-5 px-10 flex flex-col justify-center gap-10">
+            
+        </div>
+    )
+}
+
